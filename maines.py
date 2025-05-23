7846196ea3b9a16bbd00a6692febbbe81556151d
jai84750 <saritachauhan93684@gmail.com>
2025-05-23 12:08:36 +0530

    assignment-5

---
+++ maines.py
@@ -0,0 +1,22 @@
+#task-1
+
+
+a={"Ali": 80,"Sara": 90,"John": 75}
+b=input("enter the name")
+if b in a:
+    print("Marks",a[b])
+else:
+    print("not found")
+
+#task-2
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+first = numbers[:5]
+
+reverse = first[::-1]
+
+print(first)
+print(reverse)
+
